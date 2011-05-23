162ba148c7bdd98d55bf8ef69b90979c0cf08c32
pasyuk <pasyuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-23 18:04:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/flash-album-gallery/trunk@388167 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/skins.php
+++ admin/skins.php
@@ -177,6 +177,12 @@ if( isset($_GET['skin']) ) {
 	}
 }
 $type = isset($_GET['type'])? $_GET['type'] : '';
+
+if( isset($_POST['skins_refresh']) ) {
+	// upgrade plugin
+	require_once(FLAG_ABSPATH . 'admin/tuning.php');
+	flag_tune();
+}
 ?>
 <div id="slider" class="wrap">
 	<ul id="tabs" class="tabs">
@@ -227,6 +233,7 @@ $type = isset($_GET['type'])? $_GET['type'] : '';
 
 <div class="wrap">
 <h2><?php _e('Skins', 'flag'); ?>:</h2>
+<form method="post" style="float: right"><p><input type="submit" class="button" name="skins_refresh" value="<?php _e('Refresh / Update Skins', 'flag'); ?>" /></p></form>
 <p><a class="button<?php if(!$type) echo '-primary'; ?>" href="<?php echo admin_url('admin.php?page=flag-skins'); ?>"><span style="font-size: 14px;"><?php _e('image gallery skins', 'flag'); ?></span></a>&nbsp;&nbsp;&nbsp;<a class="button<?php if($type) echo '-primary'; ?>" href="<?php echo admin_url('admin.php?page=flag-skins&amp;type=m'); ?>"><span style="font-size: 14px;"><?php _e('music gallery skins', 'flag'); ?></span></a></p>
 
 <?php
