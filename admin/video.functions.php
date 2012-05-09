89a975d2ef40a27b99c68add03c0400ef44c08f9
pasyuk <pasyuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-09 20:53:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/flash-album-gallery/trunk@542063 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/video.functions.php
+++ admin/video.functions.php
@@ -59,7 +59,7 @@ function get_v_playlists($playlist_folder = '') {
 	return $flag_playlists;
 }
 
-function flagSave_vPlaylist($title,$descr,$data,$file='') {
+function flagSave_vPlaylist($title,$descr,$data,$file='',$skinaction='') {
 	global $wpdb;
 	if(!trim($title)) {
 		$title = 'default';
@@ -73,7 +73,8 @@ function flagSave_vPlaylist($title,$descr,$data,$file='') {
 
 	$flag_options = get_option('flag_options');
     $skin = isset($_POST['skinname'])? $_POST['skinname'] : 'video_default';
-    $skinaction = isset($_POST['skinaction'])? $_POST['skinaction'] : 'update';
+	if(empty($skinaction))
+    	$skinaction = isset($_POST['skinaction'])? $_POST['skinaction'] : 'update';
 	$skinpath = trailingslashit( $flag_options['skinsDirABS'] ).$skin;
 	$playlistPath = ABSPATH.$flag_options['galleryPath'].'playlists/video/'.$file.'.xml';
 	if( file_exists($playlistPath) && ($skin == $skinaction) ) {
