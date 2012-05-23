27c5c3346dfe8c4eaedd9a0501768229e2c6943f
pasyuk <pasyuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-23 13:39:04 +0000

    git-svn-id: https://plugins.svn.wordpress.org/flash-album-gallery/trunk@548054 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ skins/flat_wall_3d_demo/flat_wall_3d_demo.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Skin Name: 3D FlatWall Gallery DEMO
+Skin URI: 
+Description: 3D FlatWall for FlAGallery.<br /><strong>Compatible:</strong><li>HitCounter</li><li>Alternate jQuery gallery for iPhone / iPad and devices without flash player.</li><li>SEO optimized and support feed RSS2.</li><li>GRAND Pages - FullWindow Mode</li><strong style="color: red;">Recommended - GRAND FlAGallery v1.7 or higher.</strong>
+Author: PGC
+Author URI: http://PhotoGalleryCreator.com
+Version: 2.6
+*/
+?>
