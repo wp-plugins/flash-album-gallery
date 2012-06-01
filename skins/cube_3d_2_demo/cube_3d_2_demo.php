773172568a6a4bb495107b5b7adf442f6ce30dd8
pasyuk <pasyuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-01 14:31:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/flash-album-gallery/trunk@551821 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ skins/cube_3d_2_demo/cube_3d_2_demo.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Skin Name: Cube 3D DEMO
+Skin URI: 
+Description: Cube 3D DEMO v2.5<br /><strong>Compatible:</strong><li>HitCounter.</li><li>Alternate jQuery gallery for iPhone / iPad devices without flash player.</li><li>SEO optimized and support feed RSS2.</li><li>GRAND Pages - FullWindow Mode.</li><br /><strong style="color: red;">Recommended - GRAND FlAGallery v1.7 or higher.</strong><br />Details: FullScreen Mode (On/Off), Thumbnails Hints.
+Author: PGC
+Author URI: http://PhotoGalleryCreator.com
+Version: 2.5
+*/
+?>
