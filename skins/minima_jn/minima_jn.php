c7fbc55f23f1ba0a9e5c222a0008c00916dbda7b
pasyuk <pasyuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-25 13:07:28 +0000

    * Update Minima skin
    git-svn-id: https://plugins.svn.wordpress.org/flash-album-gallery/trunk@686966 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- skins/minima_jn/minima_jn.php
+++ skins/minima_jn/minima_jn.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
-Skin Name: Minima - Default Skin
-Version: 1.0
+Skin Name: Minima
+Version: 1.2
 uid: minima
 Skin URI: http://photogallerycreator.com/grand-flagallery/
-Description: This is NEW <b>default</b> FlAGallery skin called <b>Minima</b>. If you need more functionality or if you want to change text for SL and FS buttons - activate <b>Optima skin</b>. <p><b>Features:</b><br>* HitCounter<br>* Alternate jQuery gallery for iPhone / iPad devices without flash player<br>* SEO optimized and support feed RSS2<br>* GRAND Pages - FullWindow Mode</p><p>Details: bottom custom size thumbnails, unlimited categories, nice scrollbar.<br /><strong style="color: red;">Requirements - GRAND FlAGallery v2.0 or higher.</strong></p>
+Description:<strong>Compatible:</strong><li>HitCounter.</li><li>Alternate jQuery gallery for iPhone / iPad devices without flash player.</li><li>SEO optimized and support feed RSS2.</li><li>GRAND Pages - FullWindow Mode.</li><br />Details: bottom custom size thumbnails, unlimited categories, nice scrollbar.<br /><strong style="color: red;">Requirements - GRAND FlAGallery v2.0 or higher.</strong><br />
 Author: PGC
 Author URI: http://PhotoGalleryCreator.com
 */
